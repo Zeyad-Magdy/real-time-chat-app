4dd9f21ce4775b863f58acc0aac8eb2ef119738d
Zeyad-Magdy <zyadmagdy98@gmail.com>
2024-02-10 23:53:02 +0200

    added server

---
+++ index.js
@@ -0,0 +1,37 @@
+//npm i express
+//npm i ejs
+
+const express = require("express");
+const app = express();
+// const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 7004;
+
+
+//#region MiddleWares
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+console.log(__dirname)
+const server = app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`)
+})
+const io = require("socket.io")(server)
+
+
+//socketIO
+io.on("connection",(socket)=>{
+    console.log(`new connection ${socket}`)
+    socket.on("chatMessage",(data)=>{
+        socket.broadcast.emit("newMessage",data)
+    })
+    socket.on("disconnect",(info)=>{
+        console.log(`${socket} user is disconnecting ${info}`)
+    })
+})
+
+app.get('/',(req,res)=>{
+    res.render("index.html")
+})
+
